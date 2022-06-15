ef21ca030d85cd01b64910eb342ea8bd1ed361b0
Nolito1 <emayopasa2006@gmail.com>
2022-06-15 12:09:00 -0500

    subo la base de la pagina.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from pyexpat import model
+from statistics import mode
+from django.db import models
 
-# Create your models here.
+class BlogModel(models.Model):
+    titulo = models.CharField(max_length=100)
+    sub_titulo = models.CharField(max_length=100)
+    cuerpo = models.TextField()
+    autor = models.CharField(max_length=100)
+    fecha_creacion = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from blog.models import BlogModel
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+
+class BlogList(ListView):
+
+    model = BlogModel
+    template_name = "blog/blog_list.html"
+
+
+class BlogDetail(DetailView):
+
+    model = BlogModel
+    template_name = "blog/blog_detail.html"
+
+
+class BlogCreate(LoginRequiredMixin, CreateView):
+
+    model = BlogModel
+    success_url = reverse_lazy("blog_list")
+    fields = ["titulo", "sub_titulo", "cuerpo", "autor"]
+
+
+class BlogUpdate(LoginRequiredMixin, UpdateView):
+
+    model = BlogModel
+    success_url = reverse_lazy("blog_list")
+    fields = ["titulo", "sub_titulo", "cuerpo", "autor"]
+
+
+class BlogDelete(LoginRequiredMixin, DeleteView):
+
+    model = BlogModel
+    success_url = reverse_lazy("blog_list")
+
+
+class BlogLogin(LoginView):
+    template_name = 'blog/blog_login.html'
+    next_page = reverse_lazy("blog_list")
+
+
+class BlogLogout(LogoutView):
+    template_name = 'blog/blog_logout.html'
+
+
+    
+class SignUpView(SuccessMessageMixin, CreateView):
+  template_name = 'blog/blog_crear_cuenta_form.html'
+  success_url = reverse_lazy('blog_login')
+  form_class = UserCreationForm
+  success_message = "Your profile was created successfully"
